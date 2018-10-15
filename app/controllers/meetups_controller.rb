2ca34ac187186aaeb762e88d3de6291e04491d48
github username <tracydo11@yahoo.com>
2018-10-14 23:03:21 -0500

    updated meetups controller to allow adds

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,14 +1,29 @@
 class MeetupsController < ApplicationController
+  before_action :meetup_params, only: :create
+
   def index
-    render json: Meetup.includes(:owners), include: ['owners']
+    render json: Meetup.all
   end
 
   def show
+
   end
 
   def edit
   end
 
-  def new
+  def create
+    meetup = Meetup.new(meetup_params)
+    if meetup.valid?
+      meetup.save
+      render json: meetup
+    else
+      render json: meetup.errors
+    end
+  end
+
+  private
+  def meetup_params
+    params.require(:meetup).permit(:name, :address, :date, :time, :zip_code)
   end
 end
